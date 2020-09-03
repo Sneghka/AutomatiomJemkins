0d62512f75980b86b516482c3abe26c83a6b69d2
sneghka <sneghkan@gmail.com>
2020-09-03 22:16:45 +0300

    first commit

---
+++ FirstTestClass.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationJenkins
+{
+    [TestFixture]
+   public class FirstTestClass
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver(@"D:\Sneghka\IT\Learning\Jenkins\Drivers\chromedriver_win32_v85");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
